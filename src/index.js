16e56e93474219c685bf3f015c36f7a137f9e37b
Bluette1 <marylene.sawyer@gmail.com>
2020-10-22 15:32:30 +0200

    Add functionality for displaying data from the api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/index.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import NavBar from './layouts/navbar';
-import Content from './layouts/content';
+import displayContent from './layouts/content';
+
 
 const rootElement = document.querySelector('#root');
 NavBar.displayNavbar(rootElement);
-Content.displayContent(rootElement);
+
+displayContent(rootElement);
--- src/layouts/content.js
+++ src/layouts/content.js
@@ -1,10 +1,35 @@
-class Content {
-  static displayContent = (rootElement) => {
-    const mainContent = document.createElement('div');
-    mainContent.setAttribute('class', 'card centered-content pt-5 col-md-6 col-12');
-    mainContent.textContent = 'Main Content';
-    rootElement.append(mainContent);
-  }
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=0fc88a8e4b0bd9b97cf191933bfdcbd8';
+
+const displayContent = (rootElement) => {
+  const mainContent = document.createElement('div');
+  mainContent.setAttribute('class', 'card centered-content pt-5 col-md-6 col-12');
+  const weatherDescription = document.createElement('div');
+  weatherDescription.setAttribute('class', 'card weather-description');
+  const currWeather = document.createElement('h5');
+  currWeather.textContent = 'Current Weather';
+  currWeather.className = 'text-uppercase p-1';
+  weatherDescription.appendChild(currWeather);
+  const currTime = document.createElement('p');
+
+  displayData(currTime, weatherDescription, 'dt');
+  const weatherDetails = document.createElement('div');
+  weatherDetails.setAttribute('class', 'card weather-details');
+  weatherDetails.textContent = 'Weather details';
+
+  mainContent.append(weatherDescription);
+  mainContent.append(weatherDetails);
+  rootElement.append(mainContent);
+
+
+}
+async function displayData(currNode, parentElement, value) {
+
+  const response = await fetch(url, { mode: 'cors' });
+  const data = await response.json();
+
+  currNode.textContent = `${data[value]}`;
+  parentElement.appendChild(currNode);
+
 }
 
-export default Content;
+export default displayContent;
