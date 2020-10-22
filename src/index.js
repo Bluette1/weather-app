4acf55b05c0bf052a4c3ba26c7bbf8d9a4910f55
Bluette1 <marylene.sawyer@gmail.com>
2020-10-22 05:22:52 +0200

    Initial set up

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+const rootElement = document.querySelector('#root');
+const title = document.createTextNode('Accurate Weather Forecast App');
+rootElement.appendChild(title);
