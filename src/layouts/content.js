92cbd34de756693d0a7f621522b2958a54dba427
Bluette1 <marylene.sawyer@gmail.com>
2020-10-25 11:12:49 +0200

    Check if moment is available before displaying time fields

--- src/layouts/content.js
+++ src/layouts/content.js
@@ -15,12 +15,13 @@ async function displayData(
   const response = await fetch(url, { mode: 'cors' });
   const data = await response.json();
   if (!valueTwo) {
-    if (valueOne === 'timezone') {
+    if (valueOne === 'timezone' && moment !== undefined) {
+
       currNode.textContent = `${toDateUTCTime(data[valueOne])}`;
     }
   } else {
     currNode.textContent = `${data[valueOne][valueTwo]}`;
-    if (valueTwo === 'sunrise' || valueTwo === 'sunset') {
+    if ((valueTwo === 'sunrise' || valueTwo === 'sunset') && moment !== undefined) {
       const offset = toDateUTCTime(data.timezone).utcOffset();
       currNode.textContent = `${secsUTCToDate(data[valueOne][valueTwo], offset)}`;
     }
