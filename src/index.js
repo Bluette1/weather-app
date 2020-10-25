9f67b3aa68e9fe18f673ba3caeb1bd1d1c40a2f9
Bluette1 <marylene.sawyer@gmail.com>
2020-10-25 06:30:52 +0200

    Add loading component

--- src/index.js
+++ src/index.js
@@ -9,14 +9,36 @@ const API_KEY = process.env.APIKEY;
 const defaultCity = 'London';
 const city = defaultCity;
 const url = 'https://api.openweathermap.org/data/2.5/weather?';
+const rootElement = document.querySelector('#root');
+rootElement.classList.add('hidden');
+
 
 const start = (city, units = 'metric') => {
-  const rootElement = document.querySelector('#root');
-  rootElement.innerHTML = '';
 
+  rootElement.innerHTML = '';
+  const body = document.querySelector('body');
+  const loadingContainer = document.createElement('div');
+  loadingContainer.setAttribute('class', 'd-flex justify-content-center align-items-center loading-component')
+  const loadingComponent = document.createElement('div');
+
+  loadingComponent.setAttribute('class', 'spinner-border');
+  loadingComponent.setAttribute('role', 'status');
+  loadingContainer.append(loadingComponent);
+  body.append(loadingContainer);
   const urlPath = `${url}q=${city}&appid=${API_KEY}&units=${units}`;
+
   NavBar.displayNavbar(rootElement, urlPath, units);
   displayContent(rootElement, urlPath, units);
+
+
+  window.addEventListener('load', function() {
+
+    setTimeout(() => {
+      loadingComponent.classList.add('hidden');
+      rootElement.classList.remove('hidden');
+    }, 3000);
+  });
+
   const searchBtn = document.querySelector('#search-btn');
   const inputSearch = document.querySelector('#input-search');
   const searchForm = document.querySelector('#search-form');
@@ -29,11 +51,13 @@ const start = (city, units = 'metric') => {
     }
   });
   celsius.addEventListener('click', () => {
+
     if (celsius.checked === true) {
       start(city, 'metric');
     }
   });
   searchBtn.addEventListener('click', (evt) => {
+
     evt.preventDefault();
     city = inputSearch.value;
 
